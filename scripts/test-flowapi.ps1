87bbb46482ffd6508fb4e8a5faa845407be88010
VinothKumarP-BF <vinothkumar.prabakaran@barrelfuel.com>
2026-08-13 13:21:31 +0530

    Update test-flowapi.ps1

--- scripts/test-flowapi.ps1
+++ scripts/test-flowapi.ps1
@@ -1,6 +1,6 @@
 $EnvironmentId = "24b559b0-ad76-ebfe-8eeb-07695bb8f305"
 
-$FlowTokenBody = @{
+$TokenBody = @{
     client_id     = $env:CLIENT_ID
     client_secret = $env:CLIENT_SECRET
     scope         = "https://service.flow.microsoft.com/.default"
@@ -11,14 +11,18 @@ $TokenResponse = Invoke-RestMethod `
     -Method Post `
     -Uri "https://login.microsoftonline.com/$env:TENANT_ID/oauth2/v2.0/token" `
     -ContentType "application/x-www-form-urlencoded" `
-    -Body $FlowTokenBody
+    -Body $TokenBody
 
 $Headers = @{
-    Authorization = "Bearer $($TokenResponse.access_token)"
-    Accept        = "application/json"
+    Authorization       = "Bearer $($TokenResponse.access_token)"
+    Accept              = "application/json"
+    "x-ms-client-scope" = "/providers/Microsoft.ProcessSimple/environments/$EnvironmentId"
 }
 
 $FlowsUrl = "https://api.flow.microsoft.com/providers/Microsoft.ProcessSimple/environments/$EnvironmentId/flows?api-version=2016-11-01"
+
+Write-Host "Calling Flow API..."
+Write-Host $FlowsUrl
 
 $Response = Invoke-RestMethod `
     -Method Get `
@@ -26,5 +30,7 @@ $Response = Invoke-RestMethod `
     -Headers $Headers
 
 $Response.value |
-    Select-Object name,@{N='DisplayName';E={$_.properties.displayName}} |
+    Select-Object `
+        name,
+        @{N='DisplayName';E={$_.properties.displayName}} |
     Format-Table -AutoSize
