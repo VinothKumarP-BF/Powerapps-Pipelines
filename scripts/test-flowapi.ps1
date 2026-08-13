62d83f76c4143175d7651996792b25c010cad6aa
VinothKumarP-BF <vinothkumar.prabakaran@barrelfuel.com>
2026-08-13 13:41:56 +0530

    Update test-flowapi.ps1

--- scripts/test-flowapi.ps1
+++ scripts/test-flowapi.ps1
@@ -53,7 +53,7 @@ $TokenResponse = Invoke-RestMethod `
 
 $AccessToken = $TokenResponse.access_token
 
-Write-Host "Token Retrieved : $(-not :IsNullOrWhiteSpace($AccessToken))"
+Write-Host "Token Retrieved : $(-not [string]::IsNullOrWhiteSpace($AccessToken))"
 Write-Host "Token Length    : $($AccessToken.Length)"
 Write-Host ""
 
